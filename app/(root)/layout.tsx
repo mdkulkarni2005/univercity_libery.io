9dc68b55f012e016aee2aff6084b800736b3365a
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-01-14 10:11:41 +0530

    Refactor layout component and add Header component
    - Refactor the layout component to accept children as a prop and update the layout structure
    - Add a new Header component with navigation links

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Header from "@/components/Header";
+import { ReactNode } from "react";
 
-const layout = () => {
+const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>layout</div>
+  <main className="root-container">
+    <div className="mx-auto max-w-7xl">
+        <Header />
+        <div className="mt-20 pb-20">
+            {children}
+        </div>
+    </div>
+  </main>
   )
-}
+};
 
-export default layout
+export default layout;
